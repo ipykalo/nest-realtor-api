8c062d73b8015d193436fb08bbc2a54425feb6e0
Ivan <ipykaloi@gmail.com>
2024-05-16 10:09:39 +0300

    updated home schema

---
+++ src/home/enums/propertry-type.enum.ts
@@ -0,0 +1,4 @@
+export enum PropertyType {
+  RESIDENTIAL = 'RESIDENTIAL',
+  CONDO = 'CONDO',
+}
--- src/home/schems/home.schema.ts
+++ src/home/schems/home.schema.ts
@@ -1,12 +1,33 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { PropertyType } from '../enums/propertry-type.enum';
 
 @Schema()
 export class Home {
   @Prop({ type: String })
-  name: string;
+  address: string;
 
   @Prop({ type: Number })
-  rooms: number;
+  bedrooms: number;
+
+  @Prop({ type: Number })
+  bathrooms: number;
+
+  @Prop({ type: String })
+  city: string;
+
+  @Prop({ type: Date })
+  createTs: Date;
+
+  @Prop({ type: Number })
+  price: number;
+
+  @Prop({ type: Number })
+  landSize: number;
+
+  @Prop({ type: String, enum: PropertyType })
+  propertyType: PropertyType;
+
+  _id?: string;
 }
 
 export const HomeSchema = SchemaFactory.createForClass(Home);
